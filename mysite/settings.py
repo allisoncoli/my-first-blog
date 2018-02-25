13d581e9f8e17207c448ad3b28eac8562492f4ed
allison.coli <allison.coli@elastic.co>
2018-02-25 14:07:46 -0800

    Fixed Allowed Sites

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2m+dhy@ei#9r367igy#^vxzmkhquz*_e0*4_5xa!a7^+%(g8@s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
